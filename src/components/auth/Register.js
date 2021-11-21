b5d1dffc305a3f61ac005641a0364c58c7f2e181
john hanna <jhanna60@hotmail.com>
2021-11-21 01:11:01 +0000

    initial commit

---
+++ src/components/auth/Register.js
@@ -0,0 +1,128 @@
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { Link, Navigate } from "react-router-dom";
+import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
+import PropTypes from "prop-types";
+
+const Register = ({ setAlert, register, isAuthenticated }) => {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    password2: "",
+  });
+
+  const { name, email, avatar, password, password2 } = formData;
+
+  const onChange = (e) =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      setAlert("Passwords do not match", "danger");
+    } else if (!validateWebsiteUrl(avatar)) {
+      setAlert(
+        "Avatar isnt a proper URL starting with http: or https:",
+        "danger"
+      );
+    } else {
+      register({ name, email, avatar, password });
+    }
+  };
+
+  const validateWebsiteUrl = (websiteUrl) => {
+    if (!websiteUrl) {
+      return true;
+    }
+    const urlRegEx = /^$|^(http|https):\/\/[^ "]+$/.test(
+      String(websiteUrl).toLowerCase()
+    );
+    return urlRegEx;
+  };
+
+  if (isAuthenticated) {
+    return <Navigate to="/dashboard" />;
+  }
+
+  return (
+    <section className="container">
+      <h1 className="large text-primary">Sign Up</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Create Your Account
+      </p>
+      <form className="form" onSubmit={onSubmit}>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="text"
+            placeholder="Avatar URL"
+            name="avatar"
+            value={avatar}
+            onChange={onChange}
+          />
+          <small className="form-text">
+            This allows you to set an avatar using a url link to a jpg/png, if
+            you leave it blank you will get a default
+          </small>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Confirm Password"
+            name="password2"
+            value={password2}
+            onChange={onChange}
+            required
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Register" />
+      </form>
+      <p className="my-1">
+        Already have an account? <Link to="/login">Sign In</Link>
+      </p>
+    </section>
+  );
+};
+
+Register.propTypes = {
+  setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated,
+});
+
+export default connect(mapStateToProps, { setAlert, register })(Register);
